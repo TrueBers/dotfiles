f0819f0329970d79b3e3ce9f09c394e2424676da
Dmitry Mostovenko <trueberserker@gmail.com>
2016-09-21 18:12:24 +0300

    add chromium, fontconfig, etc

--- bash/.bashrc
+++ bash/.bashrc
@@ -6,4 +6,18 @@
 [[ $- != *i* ]] && return
 
 alias ls='ls --color=auto'
+alias ll='ls -l'
+alias grep='grep --color=auto'
+
 PS1='[\u@\h \W]\$ '
+
+export EDITOR="vim"
+
+# ignorespace and ignoredups
+export HISTCONTROL=ignoreboth
+
+# fix line overlapping
+shopt -s checkwinsize
+
+# pkgfile hook
+source /usr/share/doc/pkgfile/command-not-found.bash
---
+++ install_bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# install must have packages
+pacman -S --needed btrfs-progs intel-ucode git htop sudo vim stow haveged pkgfile openssh mc \
+	  base-devel \
+	  gcc clang make cmake strace lsof
+
+# Entropy source
+systemctl enable haveged
+
+# DNS for systemd resolver
+systemctl enable systemd-resolved
+ls -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
+# Enable NTP sync
+timedatectl set-ntp 1
+
+# Pkgfile updates
+systemctl enable pkgfile-update.timer
